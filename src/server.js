8b310ba03254d64f1913d9a532cba4f1032f7238
Rikrown <20201100045@pq.uenf.br>
2026-02-09 18:03:10 -0300

    começando o banco de dados

--- src/server.js
+++ src/server.js
@@ -1,20 +1,48 @@
-import express from 'express';
-import skillRoutes from './routes/skills.js';
+const mysql = require('mysql2');
 
-// Banco de dados que contém TODAS (sim TODAS) as habilidades
-
-const app = express();
-app.use(express.json());
-
-app.use('/skills', skillRoutes);
-
-app.listen(3000, () => {
-  console.log('API rodando em http://localhost:3000');
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'vos12345',
+  database: 'bancoElementalia' 
 });
 
+connection.connect((err) => {
+  if (err) {
+    console.error("Erro ao conectar:", err);
+  } else {
+    console.log("Conexão estabelecida com sucesso!");
 
-//histórico de rolagens
-import rollRoutes from './routes/roll.js';
+    // Enviando o comando para criar o banco
+    connection.query("CREATE DATABASE IF NOT EXISTS bancoElementalia", (err, result) => {
+      if (err) {
+        console.log("Erro ao criar o banco: ", err);
+      } else {
+        console.log("Banco de dados pronto para o Elementalia!");
+        
+        const sqlTabela = `
+        CREATE TABLE IF NOT EXISTS personagens (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        nome VARCHAR(50),
+        elemento VARCHAR(50),
+        vidamax INT,
+        folegomax INT,
+        forca INT,
+        agilidade INT,
+        constituicao INT,
+        inteligencia INT,
+        carisma INT,
+        mente INT
+      )`;
 
-app.use('/roll', rollRoutes);
-
+      connection.query(sqlTabela, (err, result) => {
+          if (err) {
+            console.log("Erro ao criar a tabela:", err);
+          } else {
+            console.log("Tabela 'personagens' pronta para o jogo!");
+          }
+        }); // Fecha o query da tabela
+      } 
+    }); // Fecha o query do banco
+  }
+});
